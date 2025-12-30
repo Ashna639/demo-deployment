3296155ec15b91556fc164009d051a0ca6eec3f0
Ashna639 <ashnaa639@gmail.com>
2025-12-30 14:29:20 +0530

    cors allowed origin changed

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -122,8 +122,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
+    "*", "https://demo-deployment-two-bice.vercel.app/"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
